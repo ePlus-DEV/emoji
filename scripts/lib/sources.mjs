aa3c888d856545875f9dea8be739b9d7b329bec7
David Nguyen <h250694@gmail.com>
2026-09-11 13:18:12 +0700

    feat: add Noto Emoji source config

--- scripts/lib/sources.mjs
+++ scripts/lib/sources.mjs
@@ -6,7 +6,9 @@ export const SOURCES = {
     repo: 'openmoji',
     branch: 'master',
     assetPrefix: 'color/svg/',
+    filenameStyle: 'openmoji',
     license: 'CC-BY-SA-4.0',
+    attribution: 'OpenMoji',
     homepage: 'https://openmoji.org/'
   },
   twemoji: {
@@ -16,7 +18,21 @@ export const SOURCES = {
     repo: 'twemoji',
     branch: 'main',
     assetPrefix: 'assets/svg/',
+    filenameStyle: 'twemoji',
     license: 'CC-BY-4.0',
+    attribution: 'Twemoji contributors',
     homepage: 'https://github.com/jdecked/twemoji'
+  },
+  noto: {
+    id: 'noto',
+    label: 'Noto Emoji',
+    owner: 'googlefonts',
+    repo: 'noto-emoji',
+    branch: 'main',
+    assetPrefix: 'svg/',
+    filenameStyle: 'noto',
+    license: 'OFL-1.1',
+    attribution: 'Google / Noto Emoji contributors',
+    homepage: 'https://github.com/googlefonts/noto-emoji'
   }
 };
